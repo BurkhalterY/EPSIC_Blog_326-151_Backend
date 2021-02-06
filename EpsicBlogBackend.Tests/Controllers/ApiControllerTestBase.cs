db62f816a8d6d4b362197e2fcf5099052a6b882f
BurkhalterY <33062656+BurkhalterY@users.noreply.github.com>
2021-02-06 21:19:36 +0100

    Make 10 tests

---
+++ EpsicBlogBackend.Tests/Controllers/ApiControllerTestBase.cs
@@ -0,0 +1,127 @@
+﻿using EpsicBlogBackend.Data;
+using EpsicBlogBackend.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace EpsicBlogBackend.Tests.Controllers
+{
+    public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<BlogDataContext>));
+                if (descriptor == null) return;
+                services.Remove(descriptor);
+
+                services.AddDbContext<BlogDataContext>(options =>
+                {
+                    options.UseInMemoryDatabase("EpsicBlogBackend");
+                });
+
+                var sp = services.BuildServiceProvider();
+
+                using (var scope = sp.CreateScope())
+                {
+                    var scopedServices = scope.ServiceProvider;
+                    var db = scopedServices.GetRequiredService<BlogDataContext>();
+                    var logger = scopedServices.GetRequiredService<ILogger<CustomWebApplicationFactory<TStartup>>>();
+
+                    db.Database.EnsureCreated();
+
+                    try
+                    {
+                        ResetInMemoryDatabase(db);
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.LogError(ex, ex.Message);
+                    }
+                }
+            });
+        }
+
+        private static void ResetInMemoryDatabase(BlogDataContext db)
+        {
+            db.RemoveRange(db.Users);
+            db.SaveChanges();
+
+            db.AddRange(new List<User> {
+                new User { Id = 1, Username = "Yannis", IsAdmin = true },
+                new User { Id = 2, Username = "Bob" },
+                new User { Id = 3, Username = "Michel" },
+            });
+            db.SaveChanges();
+        }
+    }
+
+    public class ApiControllerTestBase : CustomWebApplicationFactory<Startup>
+    {
+        private CustomWebApplicationFactory<Startup> _factory;
+        private HttpClient _client;
+
+        [TestInitialize]
+        public void SetupTest()
+        {
+            _factory = new CustomWebApplicationFactory<Startup>();
+            _client = _factory.CreateClient();
+        }
+
+        protected async Task<HttpResponseMessage> GetAsync(string url)
+        {
+            return await _client.GetAsync(url);
+        }
+
+        protected async Task<T> GetAsync<T>(string url)
+        {
+            var response = await _client.GetAsync(url);
+
+            var body = await response.Content.ReadAsStringAsync();
+
+            return JsonSerializer.Deserialize<T>(body);
+        }
+
+        protected async Task<HttpResponseMessage> PostBasicAsync<T>(string url, T body)
+        {
+            return await _client.PostAsJsonAsync(url, body);
+        }
+
+        protected async Task<HttpResponseMessage> PostFileAsync(string url, byte[] file)
+        {
+            MultipartFormDataContent content = new MultipartFormDataContent();
+            content.Add(new ByteArrayContent(file), "file", "filename");
+            return await _client.PostAsync(url, content);
+        }
+
+        protected async Task<T> PostAsync<T>(string url, T body)
+        {
+            return await PostAsync<T, T>(url, body);
+        }
+
+        protected async Task<U> PostAsync<T, U>(string url, T body)
+        {
+            var response = await _client.PostAsJsonAsync(url, body);
+
+            return await response.Content.ReadFromJsonAsync<U>();
+        }
+
+        protected async Task<HttpResponseMessage> DeleteAsync(string url)
+        {
+            var response = await _client.DeleteAsync(url);
+
+            return response.EnsureSuccessStatusCode();
+        }
+    }
+}
---
+++ EpsicBlogBackend.Tests/Controllers/UsersControllerTests.cs
@@ -0,0 +1,150 @@
+﻿using EpsicBlogBackend.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EpsicBlogBackend.Tests.Controllers
+{
+    [TestClass]
+    public class UsersControllerTests : ApiControllerTestBase
+    {
+        private string defaultPass = "Asdf1234.";
+
+        [TestMethod]
+        public async Task GetAll()
+        {
+            // Arrange
+
+            // Act
+            var response = await GetAsync<List<User>>("/users");
+
+            // Assert
+            Assert.AreEqual(3, response.Count);
+        }
+
+        [TestMethod]
+        public async Task GetSubzero()
+        {
+            // Arrange
+
+            // Act
+            var response = await GetAsync("/users/-1");
+
+            // Assert
+            Assert.AreEqual(StatusCodes.Status400BadRequest, (int)response.StatusCode);
+        }
+
+        [TestMethod]
+        public async Task GetNotExists()
+        {
+            // Arrange
+
+            // Act
+            var response = await GetAsync("/users/42069");
+
+            // Assert
+            Assert.AreEqual(StatusCodes.Status404NotFound, (int)response.StatusCode);
+        }
+
+        [TestMethod]
+        public async Task Add()
+        {
+            // Arrange
+            var model = new User { Username = "Test", Password = defaultPass, Passconf = defaultPass };
+
+            // Act
+            var response = await PostAsync("/users", model);
+
+            // Assert
+            Assert.AreEqual("Test", response.Username);
+        }
+
+        [TestMethod]
+        public async Task AddAlreadyExists()
+        {
+            // Arrange
+            var model = new User { Username = "Yannis", Password = defaultPass, Passconf = defaultPass };
+
+            // Act
+            var response = await PostBasicAsync("/users", model);
+
+            // Assert
+            Assert.AreEqual(StatusCodes.Status400BadRequest, (int)response.StatusCode);
+        }
+
+        [TestMethod]
+        public async Task Delete()
+        {
+            // Arrange
+
+            // Act
+            await DeleteAsync("/users/1");
+            var response = await GetAsync<List<User>>("/users");
+
+            // Assert
+            Assert.AreEqual(2, response.Count);
+        }
+
+        [TestMethod]
+        public async Task CheckPassword()
+        {
+            // Arrange
+            var modelUpdate = new UserUpdateViewModel { Password = defaultPass, Passconf = defaultPass };
+            var modelCheck = new UserCheckPasswordViewModel { Username = "Yannis", Password = defaultPass };
+
+            // Act
+            await PostAsync("/users/1", modelUpdate);
+            var response = await PostBasicAsync("/users/check_password", modelCheck);
+
+            // Assert
+            Assert.AreEqual(StatusCodes.Status200OK, (int)response.StatusCode);
+        }
+
+        [TestMethod]
+        public async Task WrongPassword()
+        {
+            // Arrange
+            var modelUpdate = new UserUpdateViewModel { Password = defaultPass, Passconf = defaultPass };
+            var modelCheck = new UserCheckPasswordViewModel { Username = "Yannis", Password = defaultPass + "-wrong" };
+
+            // Act
+            await PostAsync("/users/1", modelUpdate);
+            var response = await PostBasicAsync("/users/check_password", modelCheck);
+
+            // Assert
+            Assert.AreEqual(StatusCodes.Status403Forbidden, (int)response.StatusCode);
+        }
+
+        [TestMethod]
+        public async Task WrongPassconf()
+        {
+            // Arrange
+            var modelUpdate = new UserUpdateViewModel { Password = "Asdf1234.", Passconf = defaultPass + "-wrong" };
+
+            // Act
+            var response = await PostBasicAsync("/users/1", modelUpdate);
+
+            // Assert
+            Assert.AreEqual(StatusCodes.Status400BadRequest, (int)response.StatusCode);
+        }
+
+        [TestMethod]
+        public async Task SetAvatar()
+        {
+            //Arrange
+            var file = File.ReadAllBytes("avatar.png");
+
+            // Act
+            var response = await PostFileAsync($"/users/1/avatar", file);
+
+            // Assert
+            Assert.AreEqual(StatusCodes.Status200OK, (int)response.StatusCode);
+
+            var responseGet = await (await GetAsync($"/users/1/avatar")).Content.ReadAsByteArrayAsync();
+            Assert.IsTrue(responseGet.SequenceEqual(file));
+        }
+    }
+}
